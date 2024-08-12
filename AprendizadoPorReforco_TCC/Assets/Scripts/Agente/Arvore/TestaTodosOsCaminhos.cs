5c19d9a962b686561cc387970497d4ed45ac956e
ArthurSalesFM <sales.arthur.fm@gmail.com>
2024-08-11 23:31:07 -0300

    Atualização

--- AprendizadoPorReforco_TCC/Assets/Scripts/Agente/Arvore/TestaTodosOsCaminhos.cs
+++ AprendizadoPorReforco_TCC/Assets/Scripts/Agente/Arvore/TestaTodosOsCaminhos.cs
@@ -1,4 +1,4 @@
-using Palmmedia.ReportGenerator.Core.Parser.Analysis;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,42 +8,102 @@ public class TestaTodosOsCaminhos
     private List<string[,]> caminhosPossiveis = new List<string[,]>();
     private List<int?> pontosTotais = new List<int?>();
 
-    /*
-    public TestaTodosOsCaminhos(string[,] matriz)
-    {
-        this.shapeBase = (string[,])matriz.Clone();
-        this.caminhosPossiveis = new List<string[,]>();
-        this.pontosTotais = new List<int?>();
-
-        int tamanho = matriz.GetLength(0);
-
-
-        // Inicializa a matriz com espaços em branco
-        for (int i = 0; i < tamanho; i++)
-        {
-            for (int j = 0; j < tamanho; j++)
-            {
-                matriz[i, j] = " ";
-            }
-        }
-        GerarTodosCaminhos(matriz);
-
-        Debug.Log("*************************** %TODOS OS CAMINHOS% ***************************\n\n");
-        this.ImprimirCaminhosPossiveis();
-    }
-    */
-
     public void setaValores(string[,] matriz)
     {
-        this.shapeBase = matriz;
-
+        this.setShapeBase(matriz);
 
         if (this.caminhosPossiveis.Count == 0)
         {
             GerarTodosCaminhos(matriz);
         }
 
+        Debug.Log("*************************** %TODOS OS CAMINHOS% ***************************\n\n");
+        this.ImprimirCaminhosPossiveis();
+    }
+
+    public string[,] getShapeBase()
+    {
+        return this.shapeBase;
+    }
+
+    private void setShapeBase(string[,] matrizBase)
+    {
+        this.shapeBase = matrizBase;
+    }
+
+    private void GerarTodosCaminhos(string[,] matriz)
+    {
+        string[,] caminho = new string[matriz.GetLength(0), matriz.GetLength(1)];
+
+        this.caminhosPossiveis.Add(criaCaminho(caminho, 0, caminho.GetLength(1)) );
+    }
+
+    private string[,] criaCaminho(string[,] matriz, int inicio, int fim)
+    {
+
+
         
+        return caminhos;
+    }
+
+    private void ImprimirCaminhosPossiveis()
+    {
+
+        //string caminho = "";
+        string saidaGeral = "";
+        for (int inicio = 0; inicio < this.caminhosPossiveis.Count; inicio++)
+        {
+            //Debug.Log("Caminho " + inicio);
+            saidaGeral += ImprimirMatriz(this.caminhosPossiveis[inicio]);
+        }
+
+        saidaGeral += "\n\n";
+
+        Debug.Log(saidaGeral);
+
+    }
+
+    private string ImprimirMatriz(string[,] matriz)
+    {
+        string saida = "";
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                saida += matriz[i, j] + " | ";
+            }
+            saida += "\n";
+        }
+
+        saida += "\n*";
+
+        return saida;
+    }
+
+}
+
+
+
+/*
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestaTodosOsCaminhos
+{
+    private string[,] shapeBase;
+    private List<string[,]> caminhosPossiveis = new List<string[,]>();
+    private List<int?> pontosTotais = new List<int?>();
+
+    public void setaValores(string[,] matriz)
+    {
+        this.setShapeBase(matriz);
+
+
+        if (this.caminhosPossiveis.Count == 0)
+        {
+            GerarTodosCaminhos(matriz);
+        }
 
         Debug.Log("*************************** %TODOS OS CAMINHOS% ***************************\n\n");
         this.ImprimirCaminhosPossiveis();
@@ -54,72 +114,61 @@ public class TestaTodosOsCaminhos
         return this.shapeBase;
     }
 
-    public void setShapeBase(string[,] matrizBase)
+    private void setShapeBase(string[,] matrizBase)
     {
         this.shapeBase = matrizBase;
     }
 
     private void GerarTodosCaminhos(string[,] matriz)
     {
-        // Faz uma cópia da matriz inicial para evitar modificações indesejadas
-        //string[,] matrizCopia = (string[,])matriz.Clone();asdadsads
-        ExplorarCaminhos(matriz, 0, 0);kjkjhkhs
+        string[,] caminho = new string[matriz.GetLength(0), matriz.GetLength(1)];              
+     
+        this.caminhosPossiveis = CriarTodosOsCaminhos(caminho, 0, caminho.GetLength(0));//kjkjhkhs)
+        //this.ImprimirCaminhosPossiveis();
     }
 
-    private void ExplorarCaminhos(string[,] matriz, int linha, int coluna)
+
+    private List<string[,]> CriarTodosOsCaminhos(string[,] matriz, int inicio, int fim)
     {
-        // Marca a posição atual com "!"
-        matriz[linha, coluna] = "!";
+        List<string[,]> caminhos = new List<string[,]>();  // Lista para armazenar todas as matrizes completas
 
-        // Se todos os passos foram dados, armazena a matriz atual na lista de caminhos
-        if (VerificarTodosEspacosPreenchidos(matriz))
+        // Condição de parada: matriz com apenas uma linha
+        if (fim - inicio <= 1)
         {
-            string[,] caminhoCompleto = (string[,])matriz.Clone();
-            this.caminhosPossiveis.Add(caminhoCompleto);
-            pontosTotais.Add(null); // Adiciona um valor null à lista pontosTotais
-        }
-        else
-        {
-            // Movimentos possíveis: para baixo, para a direita, para cima, para a esquerda
-            if (linha + 1 < matriz.GetLength(0) && matriz[linha + 1, coluna] == " ")
-            {
-                ExplorarCaminhos(matriz, linha + 1, coluna);
-            }
-
-            if (coluna + 1 < matriz.GetLength(1) && matriz[linha, coluna + 1] == " ")
-            {
-                ExplorarCaminhos(matriz, linha, coluna + 1);
-            }
-
-            if (linha - 1 >= 0 && matriz[linha - 1, coluna] == " ")
-            {
-                ExplorarCaminhos(matriz, linha - 1, coluna);
-            }
-
-            if (coluna - 1 >= 0 && matriz[linha, coluna - 1] == " ")
-            {
-                ExplorarCaminhos(matriz, linha, coluna - 1);
-            }
+            // Adiciona uma cópia da matriz à lista
+            string[,] caminho = new string[matriz.GetLength(0), matriz.GetLength(1)];
+            Array.Copy(matriz, caminho, matriz.Length);
+            caminhos.Add(caminho);
+            return caminhos;
         }
 
-        // Desmarca a posição atual antes de retornar (backtracking)
-        matriz[linha, coluna] = " ";
-    }
+        // Divida a matriz em duas partes e processe cada parte
+        int meio = (inicio + fim) / 2;
 
-    private bool VerificarTodosEspacosPreenchidos(string[,] matriz)
-    {
-        // Verifica se todos os espaços na matriz estão preenchidos com "!"
+        caminhos.AddRange(CriarTodosOsCaminhos(matriz, inicio, meio));
+        caminhos.AddRange(CriarTodosOsCaminhos(matriz, meio, fim));
+
+        // Processar a matriz 'caminho' para o intervalo atual
+        string[,] caminhoAtual = new string[matriz.GetLength(0), matriz.GetLength(1)];
         for (int i = 0; i < matriz.GetLength(0); i++)
         {
             for (int j = 0; j < matriz.GetLength(1); j++)
             {
-                if (matriz[i, j] == " ")
+                if (j >= inicio && j < meio)
                 {
-                    return false;
+                    caminhoAtual[i, j] = "!";
+                }
+                else
+                {
+                    caminhoAtual[i, j] = ".";
                 }
             }
         }
-        return true;
+
+        // Adiciona a matriz 'caminhoAtual' à lista de caminhos
+        caminhos.Add(caminhoAtual);
+
+        return caminhos;
     }
 
     private void ImprimirCaminhosPossiveis()
@@ -129,12 +178,13 @@ public class TestaTodosOsCaminhos
         string saidaGeral = "";
         for(int inicio = 0; inicio < this.caminhosPossiveis.Count; inicio++)
         {
-            Debug.Log("Caminho " + inicio);
-            saidaGeral+=ImprimirMatriz(this.caminhosPossiveis[inicio]);
+            //Debug.Log("Caminho " + inicio);
+            saidaGeral += ImprimirMatriz(this.caminhosPossiveis[inicio]);
         }
 
-        Debug.Log(saidaGeral);
+        saidaGeral += "\n\n";
 
+        Debug.Log(saidaGeral);
         
     }
 
@@ -150,8 +200,9 @@ public class TestaTodosOsCaminhos
             saida += "\n";
         }
 
-        saida += "\n*\n";
+        saida += "\n*";
 
         return saida;
     }
 }
+*/
